7f6e9078c9dc30f7ea250602aa12945b8169721d
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-10-01 06:37:56 +0530

    Add project files.

---
+++ MinimalApiExamples/Extensions/MapProductsApiExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace MinimalApiExamples.Extensions;
+
+public static class MapProductsApiExtension
+{
+    public static RouteGroupBuilder MapProductsApi(this RouteGroupBuilder group)
+    {
+        group.MapGet("", () => Results.Ok("Get all products"));
+        group.MapGet("{id}", (int id) => Results.Ok($"Get product with ID: {id}"));
+        group.MapPost("", () => Results.Ok("Create a new product"));
+        group.MapPut("{id}", (int id) => Results.Ok("Update product"));
+        group.MapDelete("{id}", (int id) => Results.Ok("Delete product"));
+
+        return group;
+    }
+}
---
+++ MinimalApiExamples/Program.cs
@@ -0,0 +1,26 @@
+using MinimalApiExamples.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGroup("/api/products")
+.MapProductsApi()
+.WithTags("product api");
+
+app.Run();
+
